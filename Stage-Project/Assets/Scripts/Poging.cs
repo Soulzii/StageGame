3e9a2163cbbddbb6e5d9e11713fab6e89a3f2ea0
Meow <Frankyberntsen@gmail.com>
2015-11-27 12:18:53 +0100

    stagecommit 2

---
+++ Stage-Project/Assets/Scripts/Poging.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Poging{
+
+    public float msAtEnd;
+    public float vKrachtStart;
+    public float dKrachtStart;
+    public float massaStart;
+    public float Score;
+    public string Reason;
+
+    public Poging(float msAtEnd, float vKStart, float dKStart, float mStart, float score, string Reason)
+    {
+        this.msAtEnd = msAtEnd;
+        this.vKrachtStart = vKStart;
+        this.dKrachtStart = dKStart;
+        this.massaStart = mStart;
+        this.Score = score;
+        this.Reason = Reason;
+    }
+
+    public Poging()
+    {
+
+    }
+}
